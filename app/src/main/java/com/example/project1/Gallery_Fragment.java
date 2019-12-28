c71f9fec09ce1152dda033ede0713bfd0f455d1e
Son <guswns8431@gmail.com>
2019-12-28 23:08:09 +0900

    일단 됐어 이 자식아 ㅎㅎ

--- app/src/main/java/com/example/project1/Gallery_Fragment.java
+++ app/src/main/java/com/example/project1/Gallery_Fragment.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -12,24 +14,39 @@ import android.view.ViewGroup;
 import android.widget.GridView;
 import android.widget.TextView;
 
+import static com.example.project1.MyApplication.getAppContext;
+
 public class Gallery_Fragment extends Fragment {
+
+    MyApplication app;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        app = (MyApplication) getAppContext();
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_gallery_,container,false);
 
         int img[]= {
                 R.drawable.a,R.drawable.a,R.drawable.a,R.drawable.a,R.drawable.a,R.drawable.a,R.drawable.a,R.drawable.a,R.drawable.a,R.drawable.a,
                 R.drawable.a,R.drawable.a,R.drawable.a,R.drawable.a,R.drawable.a,R.drawable.a,R.drawable.a,R.drawable.a,R.drawable.a,R.drawable.a,
         };
+
         MyAdapter adapter = new MyAdapter (
-                getApplicationContext(),
+                app = (MyApplication) getAppContext(),
                 R.layout.row,
                 img);
 
-        GridView gv = (GridView)findViewById(R.id.gridView1);
+        GridView gv = (GridView)view.findViewById(R.id.gridView1);
         gv.setAdapter(adapter);
 
+        return view;
     }
 }
-}
+
