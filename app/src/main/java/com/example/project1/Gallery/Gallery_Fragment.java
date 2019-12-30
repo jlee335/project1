374c60776056259c2eae2828caafdc6291b9fa55
Son <guswns8431@gmail.com>
2019-12-30 20:19:42 +0900

    Gallery package 추가 - son

--- app/src/main/java/com/example/project1/Gallery/Gallery_Fragment.java
+++ app/src/main/java/com/example/project1/Gallery/Gallery_Fragment.java
@@ -1,18 +1,16 @@
-package com.example.project1;
+package com.example.project1.Gallery;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
-import android.widget.TextView;
+
+import com.example.project1.MyApplication;
+import com.example.project1.R;
 
 import static com.example.project1.MyApplication.getAppContext;
 
--- app/src/main/java/com/example/project1/Gallery/MyAdapter.java
+++ app/src/main/java/com/example/project1/Gallery/MyAdapter.java
@@ -1,4 +1,4 @@
-package com.example.project1;
+package com.example.project1.Gallery;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+
+import com.example.project1.R;
 
 public class MyAdapter extends BaseAdapter {
     Context context;
